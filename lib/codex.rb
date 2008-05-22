3c27b54a468cc8b3aaac2048480442ea16011485
Dave Thomas <dave@pragprog.com>
2008-05-21 23:12:12 -0500

    Merged in cth plugin capability. Modified to make plugins options, so that if they require a gem that's not locally installed, codex itself still loads

--- lib/codex.rb
+++ lib/codex.rb
@@ -1,5 +1,6 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+base_dir = File.dirname(__FILE__)
+$:.unshift(base_dir) unless
+  $:.include?(base_dir) || $:.include?(File.expand_path(base_dir))
 
 module Codex; end
 
@@ -7,6 +8,16 @@ require 'codex/content'
 require 'codex/pressie'
 require 'codex/filter'
 
-# Filters:
-require 'codex/filters/code_filter'
-require 'codex/filters/ritex_filter'
+# Filters:    
+
+# If a filter fails to load, simply ignore it (it may be a missing local gem, such
+# as ritex). This means that folks who write filters must ensure that their source
+# throws an exception should it fail to find all its dependencies
+
+Dir.glob(File.join(base_dir, 'codex/filters/*.rb')) do |filter|
+  begin
+    require(filter)
+  rescue Exception => e
+    ;
+  end
+end
