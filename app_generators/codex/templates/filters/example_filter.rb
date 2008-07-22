9dc6b15b5a2ad1098061b13c00a6d9c81e5f155c
Brandon Keepers <brandon@collectiveidea.com>
2008-07-22 12:14:38 -0400

    attack of the indentation nazis

--- app_generators/codex/templates/filters/example_filter.rb
+++ app_generators/codex/templates/filters/example_filter.rb
@@ -1,13 +1,13 @@
 # This is _my_ example filter. Define your own ;-)
 
 class TestFilter < Codex::Filter
-        tag :test
+  tag :test
 
-        def filter_inline(text,args)
-                text.reverse
-        end
-        
-        def filter_single(args)
-            args.reverse
-        end
+  def filter_inline(text,args)
+    text.reverse
+  end
+
+  def filter_single(args)
+    args.reverse
+  end
 end
